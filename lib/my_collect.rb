28dbd85b338d80f04452c0913ccd370b3710694d
aurel fontaine <ajfame11@gmail.com>
2020-04-01 02:38:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collect)
   i = 0
   new_collection = []
   while i < collection.length
-  new_collection << yield(collection)
+  new_collection << yield(collection[i])
   i = i + 1
 end
 new_collection
